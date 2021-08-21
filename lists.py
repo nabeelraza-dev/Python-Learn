e596e9c4794ca437c5ec71bcffd2ece44911219c
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-21 12:17:54 +0500

    lists in python

---
+++ lists.py
@@ -0,0 +1,18 @@
+list1 = ['Zaryab', 23, 5.9, 'Pakistan']
+
+for element in list1:
+    print(element)
+print('\n')
+# Accessing value in a list
+print(list1[0])
+print(list1[0:3])
+
+# Updating list
+list1[0] = 'Ali'
+list1[1] = 22
+list1[2] = 6.3
+print(list1)
+
+# Deleting list elements\
+del list1[2]
+print(list1)
