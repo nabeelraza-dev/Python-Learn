6aaf9734465e3ce392a53d02c62d160bdfbb0aba
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-21 14:26:28 +0500

    modules in python

---
+++ Modlue.py
@@ -0,0 +1,4 @@
+import partModule as pm
+
+list1 = ["Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"]
+print(pm.insertionSort(list1))
---
+++ partModule.py
@@ -0,0 +1,11 @@
+# make insertion sort funciton in python
+# use python3
+def insertionSort(alist):
+    for index in range(1,len(alist)):
+        currentvalue = alist[index]
+        position = index
+        while position>0 and alist[position-1]>currentvalue:
+            alist[position]=alist[position-1]
+            position = position-1
+        alist[position]=currentvalue
+    return alist
