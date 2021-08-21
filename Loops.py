0e3abe62644541c32e0da6c9df1f58ccdba7d67a
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-21 11:48:15 +0500

    For loop

---
+++ Loops.py
@@ -0,0 +1,17 @@
+# While Loop
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1
+
+# For Loop
+for letter in 'Python':     # traversal of a string sequence
+   print ('Current Letter :', letter)
+
+
+# Right Triangle in Python
+for i in range(6):
+    for j in range(i+1):
+        print("#" , end="")
+    print("")
