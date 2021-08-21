3f0a84285b3695530040203398afd662250f9acf
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-21 11:58:49 +0500

    strings

---
+++ Strings.py
@@ -0,0 +1,12 @@
+var1 = 'Hello World!'
+print ("Updated String :- ", var1[:6] + 'Python')
+
+
+para_str = """this is a long string that is made up of
+several lines and non-printable characters such as
+TAB ( \t ) and they will show up that way when displayed.
+NEWLINEs within the string, whether explicitly given like
+this within the brackets [ \n ], or just a NEWLINE within
+the variable assignment will also show up.
+"""
+print (para_str)
